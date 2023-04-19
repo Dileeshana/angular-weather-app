40bb57f6abf71b953893794c2cb3a9b793a324c3
Dileeshana <dileeshana2000@gmail.com>
2023-04-19 20:46:17 +0530

    API connecting, interface all done

---
+++ weatherApp/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from './services/weather.service';
+import { WeatherData } from './models/weather.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+
+  constructor(private weatherService: WeatherService) {
+
+  }
+
+  weatherData?: WeatherData;
+
+  ngOnInit(): void {
+    this.weatherService.getWeatherData('Wellington')
+    .subscribe({
+      next: (response) => {
+        this.weatherData= response;
+        console.log(response);
+      }
+    })
+  }
+  
+}
---
+++ weatherApp/src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    weatherAPIBaseUrl: 'https://openweather43.p.rapidapi.com/weather',
+    XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue: 'openweather43.p.rapidapi.com',
+    XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderValue: 'caf3224cc5msh2d424036bcdd4f8p197c19jsn6baafe53f428'
+};
