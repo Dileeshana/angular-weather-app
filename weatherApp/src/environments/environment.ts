07777efeca158ea117445428c02df6fab959e6f9
Dileeshana <79755959+Dileeshana@users.noreply.github.com>
2023-04-28 15:00:58 +0530

    Update environment.ts
    leak fixed

--- weatherApp/src/environments/environment.ts
+++ weatherApp/src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
     production: true,
     weatherAPIBaseUrl: 'https://openweather43.p.rapidapi.com/weather',
-    XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    // XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
     XRapidAPIHostHeaderValue: 'openweather43.p.rapidapi.com',
-    XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+    // XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
     XRapidAPIKeyHeaderValue: '6ff04c4376msh11661e82a6c4eccp1fff3ajsnd51e36ff67a8'
 };
 
